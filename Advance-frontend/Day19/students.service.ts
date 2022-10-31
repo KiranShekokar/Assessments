8a15f5fac9a50dbbb732a2946c938fed614d8484
KiranShekokar <113107871+KiranShekokar@users.noreply.github.com>
2022-10-31 21:55:54 +0530

    Add files via upload

--- Advance-frontend/Day19/students.service.ts
+++ Advance-frontend/Day19/students.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,7 +6,7 @@ import { Injectable } from '@angular/core';
 })
 export class StudentsService {
 
-  constructor() { }
+  constructor(private httpObj : HttpClient) { }
   empArr:any[] = [
     {sno:1,ename:"Emp1",performace:1},
     {sno:2,ename:"Emp2",performace:2},
@@ -20,4 +21,33 @@ export class StudentsService {
   getStudentArr():any[]{
     return this.empArr;
   }
+
+  //studentArr:any[]=[];
+
+  getStudent():any{
+    let url = "http://localhost:3000/students";
+   return this.httpObj.get(url);
+   }
+   selectStudent(id:number):any{
+    let url = "http://localhost:3000/students/"+id;
+    return this.httpObj.get(url);
+   }
+
+  updateStudent_data(studentObj:any):any{
+
+    let url = "http://localhost:3000/students/"+studentObj.studentId;
+    return this.httpObj.put(url,studentObj);
+
+  }
+
+  addStudent(studentObj:any):any{
+    let url = "http://localhost:3000/students";
+    return this.httpObj.post(url , studentObj);
+  }
+
+  deleteStudent_Data(id:number):any{
+    let url = "http://localhost:3000/students/"+id;
+    return this.httpObj.delete(url);
+  }
+
 }
